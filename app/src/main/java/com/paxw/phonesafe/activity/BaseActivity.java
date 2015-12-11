84340da7090f1859bc35c61d14accddb742f8ff5
limitlc <120706562@qq.com>
2015-12-11 17:15:05 +0800

    提交

--- app/src/main/java/com/paxw/phonesafe/activity/BaseActivity.java
+++ app/src/main/java/com/paxw/phonesafe/activity/BaseActivity.java
@@ -2,21 +2,25 @@ package com.paxw.phonesafe.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 
 /**
  * Created by Administrator on 2015/12/11.
  */
 public abstract class BaseActivity extends Activity {
+
+
     @Override
-    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
         initView();
     }
+
     protected abstract void initView();
 
     @Override
     protected void onDestroy() {
+
         super.onDestroy();
     }
 }
--- app/src/main/java/com/paxw/phonesafe/activity/SplashActivity.java
+++ app/src/main/java/com/paxw/phonesafe/activity/SplashActivity.java
@@ -1,11 +1,63 @@
 package com.paxw.phonesafe.activity;
 
-/**
- * Created by Administrator on 2015/12/11.
- */
-public class SplashActivity extends BaseActivity {
-    @Override
-    protected void initView() {
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.paxw.phonesafe.myapplication.R;
+import com.paxw.phonesafe.utils.ToastUtil;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends BaseActivity{
+
+    private ImageView image;
+    private TextView version;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+    protected void initView() {
+        setContentView(R.layout.activity_first);
+        image = (ImageView) findViewById(R.id.splash_image_view);
+        version = (TextView) findViewById(R.id.version_text);
+        version.setText("版本号:" + getVersion());
+        Timer timer = new Timer();
+        TimerTask task =new TimerTask(){
+
+            /**
+             * The task to run should be specified in the implementation of the {@code run()}
+             * method.
+             */
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        //// TODO: 跳转到下一个界面
+                        ToastUtil.showToast("时间到了要执行下一个界面了");
+                    }
+                });
+            }
+        };
+        timer.schedule(task,3000);
+
+    }
+    public String getVersion() {
+        PackageManager pm = getPackageManager();
+        try {
+            PackageInfo packageInfo = pm.getPackageInfo(getPackageName(), 0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 }
